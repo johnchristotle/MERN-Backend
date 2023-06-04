6c058443b5c6471075a222a73c12c0352982d707
Christotle Agholor <96313953+johnchristotle@users.noreply.github.com>
2023-06-04 11:55:12 -0700

    Update userController.js
    token expiration adjusted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,7 +104,7 @@ const getCurrentUser = asyncHandler(async(req, res) => {
 // Generate Token
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn:'30d'
+        expiresIn:'3m'
     })
 }
 
